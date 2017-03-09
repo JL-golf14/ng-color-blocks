16b7f449932084b01080b2052a4626fdf3e5b730
Jeremy Leif <jeremy.leif@gmail.com>
2017-03-08 19:52:11 -0600

    hard as it is

--- server/public/scripts/controllers/settings.controller.js
+++ server/public/scripts/controllers/settings.controller.js
@@ -1,5 +1,22 @@
-colorBlocks.controller('SettingsController', function($scope) {
+colorBlocks.controller('SettingsController',['DataFactory','$scope', function(DataFactory, $scope) {
   console.log("settings controller running");
+  var self = this;
+   self.colors = DataFactory.colors;
+   self.newColor='';
+
+   self.addNewColor = function(){
+
+       self.colors.push(self.newColor);
+
+   console.log(self.colors);
+return self.colors;
+};
 
 
-});
+
+
+
+
+
+
+}]);
