b29bdc4f565edc96a2c9ddd2acd7354e514a207e
Jeremy Leif <jeremy.leif@gmail.com>
2017-03-08 22:45:43 -0600

    score works, add color works, no edit or halloffame, besides me

--- server/public/scripts/controllers/game.controller.js
+++ server/public/scripts/controllers/game.controller.js
@@ -4,7 +4,8 @@ console.log('game controller running');
 
 var self = this;
  self.colors = DataFactory.colors;
-// 
+ self.highScore= DataFactory.highScore;
+//
 // start game
 init();
 
@@ -12,15 +13,23 @@ init();
 function init() {
   self.messageText = "";
   self.currentColor = self.colors[randomNumber(0, self.colors.length - 1)];
-  self.colorPrompt = 'Can you find the ' + self.currentColor + ' block?'
+  self.colorPrompt = 'Can you find the ' + self.currentColor + ' block?';
+  self.highScore;
 }
 
 // click handler for guessing colors
 self.handleInput = function(clickedColor) {
   if(clickedColor === self.currentColor) {
+    self.highScore += parseInt((self.colors.length/2)*100);
+    console.log(self.highScore);
     alert('You got it!\n\nNow try another!');
     init();
-  } else {
+  } else if (self.highScore<-10000) {
+    self.messageText = 'Are you Color Blind????';
+  }
+  else {
+    self.highScore -= parseInt((self.colors.length/4)*100);
+    console.log(self.highScore);
     self.messageText = 'Oh no! You guessed wrong!';
   }
 }
--- server/public/scripts/controllers/settings.controller.js
+++ server/public/scripts/controllers/settings.controller.js
@@ -1,18 +1,24 @@
 colorBlocks.controller('SettingsController',['DataFactory','$scope', function(DataFactory, $scope) {
   console.log("settings controller running");
   var self = this;
-   self.colors = DataFactory.colors;
-   self.newColor='';
 
-   self.addNewColor = function(){
+     self.colors = DataFactory.colors;
+     self.newColor='';
+    self.$index=''
+   self.addNewColor = function($index,colors){
 
        self.colors.push(self.newColor);
 
    console.log(self.colors);
 return self.colors;
 };
-
-
+// self.remove = function($index,colors){
+//
+//     self.colors.splice($index,colors);
+// self.colors.push(self.colors.input);
+// console.log(self.colors);
+// return self.colors;
+// };
 
 
 
